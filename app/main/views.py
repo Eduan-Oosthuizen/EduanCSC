7db04cd9ff285d5816f39b40eb055a09f934b353
Eduan Oosthuizen <eduan.edoc@gmail.com>
2016-02-18 16:22:27 +0200

    Addition of Bokeh code to render simple scatter plot (with box zoom), embedded in existing Flask structure.

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,12 @@ from flask import render_template, session, redirect, url_for, flash
 from . import main
 from .forms import UploadForm
 from werkzeug.utils import secure_filename
+from pandas import read_csv
+from numpy import linspace, exp
+from pandas import DataFrame
+from bokeh.plotting import figure
+from bokeh.embed import components
+
 
 @main.route('/', methods=['GET'])
 def index():
@@ -25,3 +31,33 @@ def upload():
     else:                                        # Now main.upload used as the function is relevant to a spec blueprint
         filename = None
     return render_template('upload.html', form=form)
+
+
+# Try to directly embed into HTML whilst still making use of Bootstrap
+@main.route('/plot', methods=['GET'])
+def plot():
+
+    # Prepare Data
+    #xdata = linspace(0, 100, 100)
+
+    #K = 3
+    #tau = 20
+    #ydata = K*(1-exp(-xdata/tau))
+
+    #Data_pandas = DataFrame({'Time': xdata,
+    #                         'Value': ydata})
+
+    #Data_pandas.to_csv('app/static/uploads/Data1.csv')
+
+    # Read data as if uploaded file is now used
+    data = read_csv('app/static/uploads/Data1.csv', sep=',', skipinitialspace=1)
+
+    xdata = data.values[:,1]    # Extract Data
+    ydata = data.values[:,2]
+
+    my_plot = figure()          # Create figure object
+    my_plot.circle(xdata, ydata)  # Add circle elements (glyphs) to the figure
+
+    script, div = components(my_plot)  # Break figure up into component HTML to be added to template
+
+    return render_template("int_scatter.html", myScript=script, myDiv=div)
