89747b34bba5bdd62c8b87ed7e93da68ae90d45f
Eduan Oosthuizen <eduan.edoc@gmail.com>
2016-03-26 09:49:28 +0200

    Added multiple data sets to single figure with callback on only one data set

--- app/main/views.py
+++ app/main/views.py
@@ -7,13 +7,13 @@ from . import main
 from .forms import UploadForm
 from werkzeug.utils import secure_filename
 from pandas import read_csv
-from numpy import linspace, exp
+from numpy import linspace, exp, sin
 from pandas import DataFrame
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import CustomJS, ColumnDataSource
 from bokeh.models.tools import BoxSelectTool, PanTool, ResetTool, WheelZoomTool, BoxZoomTool
-from bokeh.models.glyphs import Text
+
 
 
 @main.route('/', methods=['GET'])
@@ -40,43 +40,63 @@ def upload():
 @main.route('/plot', methods=['GET'])
 def plot():
 
-    # Prepare Data
-    #xdata = linspace(0, 100, 100)
+    # Read data as if uploaded file is now used, Data set 1
+    data1 = read_csv('app/static/uploads/Data1.csv', sep=',', skipinitialspace=1)
 
-    #K = 3
-    #tau = 20
-    #ydata = K*(1-exp(-xdata/tau))
+    xdata1 = data1.values[:,1]    # Extract Data
+    ydata1 = data1.values[:,2]
+    colour1 = ['navy']*len(xdata1)
 
-    #Data_pandas = DataFrame({'Time': xdata,
-    #                         'Value': ydata})
+    # Read data as if uploaded file is now used, Data set 2
+    data2 = read_csv('app/static/uploads/Data2.csv', sep=',', skipinitialspace=1)
 
-    #Data_pandas.to_csv('app/static/uploads/Data1.csv')
+    xdata2 = data2.values[:,1]
+    ydata2 = data2.values[:,2]
+    colour2 = ['green']*len(xdata2)
 
-    # Read data as if uploaded file is now used
-    data = read_csv('app/static/uploads/Data1.csv', sep=',', skipinitialspace=1)
+    # Read data as if uploaded file is now used, Data set 3
+    data3 = read_csv('app/static/uploads/Data3.csv', sep=',', skipinitialspace=1)
 
-    xdata = data.values[:,1]    # Extract Data
-    ydata = data.values[:,2]
-    colour = ['navy']*len(xdata)
+    xdata3 = data3.values[:,1]
+    ydata3 = data3.values[:,2]
+    colour3 = ['red']*len(xdata3)
 
-    sourceData = ColumnDataSource(data=dict(x=xdata, y=ydata, color=colour))
+    # Prepare Data3
+    # xdata3 = linspace(0, 100, 10)
+    # ydata3 = sin(10*xdata3)
+
+    # Data_pandas = DataFrame({'Time': xdata3,
+    #                        'Value': ydata3})
+
+    # Data_pandas.to_csv('app/static/uploads/Data3.csv')
 
 
-    my_plot = figure(tools=[BoxSelectTool(dimensions=['width'], select_every_mousemove=True), PanTool(), ResetTool(), WheelZoomTool(), BoxZoomTool()], title='Time series data',
-                     x_range=(xdata.min(), xdata.max()), y_range=(ydata.min(), ydata.max()))      # Create figure object; DEBUG: select_every_mousemove=False
-    my_plot.circle(x='x', y='y', source=sourceData,
+    # Assign read data to ColumnDataSource objects
+    sourceData1 = ColumnDataSource(data=dict(x=xdata1, y=ydata1, color=colour1))
+    sourceData2 = ColumnDataSource(data=dict(x=xdata2, y=ydata2, color=colour2))
+    sourceData3 = ColumnDataSource(data=dict(x=xdata3, y=ydata3, color=colour3))
+
+    my_plot = figure(tools=[BoxSelectTool(dimensions=['width'], select_every_mousemove=True), PanTool(), ResetTool(), WheelZoomTool(), BoxZoomTool()],
+                     title='Time series data')      # Create figure object; DEBUG: select_every_mousemove=False
+    my_plot.circle(x='x', y='y', color='color', source=sourceData1,
                    size=8, alpha=0.5)  # Add circle elements (glyphs) to the figure
+    my_plot.circle(x='x', y='y', color='color', source=sourceData2,
+                   size=8, alpha=0.5)
+    my_plot.circle(x='x', y='y', color='color', source=sourceData3,
+                   size=8, alpha=0.5)
+    my_plot.line(x='x', y='y', color='red', source= sourceData3,
+                 alpha=0.5)
 
 
     sourceFit = ColumnDataSource(data=dict(xfit=[], yfit=[]))
-    my_plot.circle(x='xfit', y='yfit', source=sourceFit, color='orange', alpha=0.6)
+    my_plot.circle(x='xfit', y='yfit', source=sourceFit, color='orange', alpha=0.3)
 
 
     # sourceAnnotate = ColumnDataSource(data=dict(text=['Foo', 'Bah'], x=[50, 50], y=[0.5, 0], x_offset=[0,0], y_offset=[0,0], text_font_size=['15pt', '15pt'],
     #                                            text_color=['orange', 'orange']))
     # my_plot.text(source=sourceAnnotate, text='text', x='x', y='y', x_offset='x_offset', y_offset='y_offset', text_font_size='text_font_size', text_color='text_color')
 
-    sourceData.callback = CustomJS(args=dict(sourceFit=sourceFit), code=("""FirstOrderEyeball(cb_obj, sourceFit)"""))
+    sourceData1.callback = CustomJS(args=dict(sourceFit=sourceFit), code=("""FirstOrderEyeball(cb_obj, sourceFit)"""))
 
 
     script, div = components(my_plot)  # Break figure up into component HTML to be added to template
